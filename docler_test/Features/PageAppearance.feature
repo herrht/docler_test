f171ee1c25efdfbef8bdb449fa8fa63bcaaaf18e
Tamas Horvath <herrht.web@gmail.com>
2020-02-02 17:39:08 +0100

    added PageAppearance feature file and initial step definitions for it

---
+++ docler_test/Features/PageAppearance.feature
@@ -0,0 +1,43 @@
+﻿@page_appearance
+Feature: PageAppearance
+	Test is important page elements are visible
+
+@HighPrio @REQ-UI-11
+Scenario: Form is visible with input field and submit button
+	When I am on the "form_page"
+	Then The "form" should be visible
+	And The "input_field" should be visible
+	And The "submit_button" should be visible
+
+@ModeratePrio @REQ-UI-01 @REQ-UI-02
+Scenario: Check page title and logo on every page
+	When I am on the "<page>"
+	Then The "title" should be "UI Testing Site"
+	And The "company_logo" should be visible
+
+	Examples:
+	| page  |
+	| home  |
+	| form  |
+	| error |
+
+@ModeratePrio @REQ-UI-01 @REQ-UI-02
+Scenario: Check page title and logo on submitted form page
+	Given I am on the "form_page"
+	When I type "Tester" in the "input_field"
+	And Click on the "submit_button"
+	Then I should be redirected to the "hello_page"
+	And The "title" should be "UI Testing Site"
+	And The "company_logo" should be visible
+
+@LowPrio @REQ-UI-04 @REQ-UI-09 @REQ-UI-10
+Scenario: Check the icon and texts on the home page
+	When I am on the "home_page"
+	Then The "home_button" should turn to active status
+	And The text should be visible in "h1_tag": "Welcome to the Docler Holding QA Department"
+	And The text should be visible in "p_tag": "This site is dedicated to perform some exercises and demonstrate automated web testing."
+
+@LowPrio @REQ-UI-06
+Scenario: Check the icon on the form page
+	When I am on the "form_page"
+	Then The "form_button" should turn to active status
---
+++ docler_test/StepDefinitions/PageAppearanceSteps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace docler_test.StepDefinitions
+{
+    [Binding]
+    public class PageAppearanceSteps
+    {
+        [When(@"I am on the ""(.*)""")]
+        public void WhenIAmOnThe(string p0)
+        {
+            //ScenarioContext.Current.Pending();
+            System.Console.WriteLine("Printer: WhenIAmOnThe - String");
+        }
+        
+        [Then(@"The ""(.*)"" should be visible")]
+        public void ThenTheShouldBeVisible(string p0)
+        {
+            //ScenarioContext.Current.Pending();
+            System.Console.WriteLine("Printer: ThenTheShouldBeVisible - String");
+        }
+        
+        [Then(@"The ""(.*)"" should be ""(.*)""")]
+        public void ThenTheShouldBe(string p0, string p1)
+        {
+            //ScenarioContext.Current.Pending();
+            System.Console.WriteLine("Printer: ThenTheShouldBe - String");
+        }
+        
+        [Then(@"The ""(.*)"" should turn to active status")]
+        public void ThenTheShouldTurnToActiveStatus(string p0)
+        {
+            //ScenarioContext.Current.Pending();
+            System.Console.WriteLine("Printer: ThenTheShouldTurnToActiveStatus - String");
+        }
+        
+        [Then(@"The text should be visible in ""(.*)"": ""(.*)""")]
+        public void ThenTheTextShouldBeVisibleIn(string p0, string p1)
+        {
+            //ScenarioContext.Current.Pending();
+            System.Console.WriteLine("Printer: ThenTheTextShouldBeVisibleIn - String");
+        }
+    }
+}
